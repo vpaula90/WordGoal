ffbcb713a599c3723fa587ec8e6a9080d0ef2a1d
Thatiana <cruz.thatiana@gmail.com>
2019-07-06 10:09:08 -0700

    added frontend/UI

--- db/schema.sql
+++ db/schema.sql
@@ -7,9 +7,11 @@ USE wordgoal_db;
 /* Create a table for all teachers */
 CREATE TABLE teachers (
 	id INT( 11 ) AUTO_INCREMENT NOT NULL,
-	last_name VARCHAR( 255) NOT NULL,
-    first_name VARCHAR( 255) NOT NULL,
-    homeroom_id VARCHAR( 255) NOT NULL,
+	last_name VARCHAR (255) NOT NULL,
+    first_name VARCHAR (255) NOT NULL,
+    homeroom_id VARCHAR ( 255) NOT NULL,
+    username VARCHAR (255) NOT NULL,
+    password VARCHAR (255), NOT NULL,
     grade_level INT (255) NOT NULL,
 	PRIMARY KEY ( id )
 );
--- routes.js
+++ routes.js
@@ -1,13 +1,15 @@
 module.exports = function(app){
 
-		const application = require('./routes/application');
-		const users = require('./routes/users');
-		const trips = require('./routes/trips');
-		const pricing = require('./routes/pricing');
+		var application = require('./routes/application');
+		var users = require('./routes/users');
+		var trips = require('./routes/trips');
+		var data = require('./routes/data');
+		
 
 		app.use('/', application);
 		app.use('/users', users);
 		app.use('/trips', trips);
-		app.use('/pricing', pricing);
+		app.use('/data', data);
+
     //other routes..
 }
