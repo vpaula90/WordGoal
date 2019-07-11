c03f4799a2d94856c5240a5d4aa59b695034d336
Karishma <karishmah@gmail.com>
2019-07-10 18:13:50 -0700

    updated files

---
+++ db/seeds-v1.sql
@@ -0,0 +1,53 @@
+USE wordgoal_db;
+
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Estevez', 'Christopher', 'LSU', 8, 100000, 1103711, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Serrano', 'Miguel', 'LSU', 8, 100000, 12541, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Lazo', 'Justin', 'LSU', 8, 100000, 0, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Lemus', 'Mary Ann', 'LSU', 8, 100000, 107000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Medina', 'Chris', 'LSU', 8, 100000, 101998, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Moraga', 'Joshua', 'LSU', 8, 100000, 283000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Navarro', 'Alondra', 'LSU', 8, 100000, 0, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Pantoja', 'Amy', 'LSU', 8, 100000, 122263, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Pastelin', 'Joshua', 'LSU', 8, 100000, 727000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Mendoza', 'Brooklynn', 'LSU', 8, 100000, 101000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('McReynolds', 'Annyka', 'UTA', 8, 100000, 48000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Mendieta', 'Jesse', 'UTA', 8, 100000, 100903, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Monge', 'Joanna', 'UTA', 8, 100000, 96000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Munoz', 'Isaac', 'UTA', 8, 100000, 0, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Paz', 'Brian', 'UTA', 8, 100000, 0, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Pena', 'Julian', 'UTA', 8, 100000, 110082, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Perez', 'Roberto', 'UTA', 8, 100000, 61412, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Vargas', 'Seanne', 'NYU', 6, 100000, 91748, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Vega', 'Ricardo', 'NYU', 6, 100000, 74130, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Walker', 'Jaque', 'NYU', 6, 100000, 26595, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Zepeda', 'Olivia', 'NYU', 6, 100000, 102000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Ordaz', 'Isela', 'NYU', 6, 100000, 126000, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Ramos', 'Samantha', 'NYU', 6, 100000, 239097, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+INSERT INTO students (last_name,  first_name, homeroom_id, grade_level, word_goal, word_count, createdAt, updatedAt)
+VALUES ('Renoj', 'Elias', 'NYU', 6, 100000, 108340, "2019-07-09 16:21:19", "2019-07-09 16:21:19");
+
